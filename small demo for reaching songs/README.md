f4e0819736a5c04d6b7e49dbbe337c4972169175
Qi Yin <yq605879396@gmail.com>
2021-04-11 14:12:51 -0400

    original

---
+++ small demo for reaching songs/README.md
@@ -0,0 +1,8 @@
+# Demo for list songs
+
+How to run it:  
+1. download mysql, php, and modified the link information in songs.php into your own   
+2. insert data into mysql  
+3. run mysql service  
+4. run php web server in this repository  
+5. open in the browser:   http://localhost:4000/songs.php
---
+++ small demo for reaching songs/songs.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title> Kumi</title>
+ </head>
+ <body>
+    <?php echo '<p>Welcome! Please input the nationality in the box below. </p>'; ?> 
+    <form action="getartist.php" method="post">
+    Nationality: <input type="text" name="nation" />
+    <input type="submit" />
+    </form>
+
+
+
+ </body>
+</html>
